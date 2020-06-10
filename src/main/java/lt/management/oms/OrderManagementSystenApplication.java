2ec0da92a496f4ac715310fc2392f68600f8f582
Paulius <paulius.gaizauskas@gmail.com>
2020-06-10 16:07:27 +0300

    Initial commit

---
+++ src/main/java/lt/management/oms/OrderManagementSystenApplication.java
@@ -0,0 +1,13 @@
+package lt.management.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSystenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementSystenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/management/oms/OrderManagementSystenApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.management.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
