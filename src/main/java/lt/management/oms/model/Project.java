668ba487544ebc688b269d49466a0d3c9cf6174b
Vilius Kasperavicius <v.kasperavicius1@gmail.com>
2020-07-14 16:19:05 +0300

    merge

--- src/main/java/lt/management/oms/model/Project.java
+++ src/main/java/lt/management/oms/model/Project.java
@@ -1,23 +1,5 @@
 package lt.management.oms.model;
 
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
->>>>>>> a3816dfbbd12d2bf721ad3456ca5e82a1545ff69
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -40,44 +22,44 @@ import java.util.concurrent.TimeUnit;
 @Entity
 public class Project extends BaseEntity {
 
-	private String name;
-	@Enumerated(EnumType.STRING)
-	private Status status;
-	// private Client client;
-	private double budget;
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "address_id", referencedColumnName = "id")
-	private Address address;
-	// private Role projectManager;
-	private long duration;
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private Date deadline;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    // private Client client;
+    private double budget;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+    // private Role projectManager;
+    private long duration;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date deadline;
 
 
-	 @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
-	 private List<Task> tasks = new ArrayList<>();
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
 
-	// For counting duration
-	@CreationTimestamp
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	private Date startDate;
-	@UpdateTimestamp
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	private Date endDate;
+    // For counting duration
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date startDate;
+    @UpdateTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endDate;
 
-	public Project(String name, Status status, double budget, Address address, Date deadline) {
-		this.name = name;
-		this.status = status;
-		this.budget = budget;
-		this.address = address;
-		this.deadline = deadline;
-	}
+    public Project(String name, Status status, double budget, Address address, Date deadline) {
+        this.name = name;
+        this.status = status;
+        this.budget = budget;
+        this.address = address;
+        this.deadline = deadline;
+    }
 
-	public long getDuration() {
-		duration = endDate.getTime() - startDate.getTime();
-		long diffInSeconds = TimeUnit.MILLISECONDS.toDays(duration);
-		return diffInSeconds;
-	}
+    public long getDuration() {
+        duration = endDate.getTime() - startDate.getTime();
+        long diffInSeconds = TimeUnit.MILLISECONDS.toDays(duration);
+        return diffInSeconds;
+    }
 
 
 }
