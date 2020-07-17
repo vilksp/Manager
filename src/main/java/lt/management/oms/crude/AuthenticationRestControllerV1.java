9127243dbb10a8a545731c31c203b74c97973132
Paulius <paulius.gaizauskas@gmail.com>
2020-07-17 15:54:11 +0300

    Enable cross origin

--- src/main/java/lt/management/oms/crude/AuthenticationRestControllerV1.java
+++ src/main/java/lt/management/oms/crude/AuthenticationRestControllerV1.java
@@ -1,5 +1,6 @@
 package lt.management.oms.crude;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/auth/")
+@CrossOrigin(origins = { "http://localhost:3000" })
 public class AuthenticationRestControllerV1 {
 
     @GetMapping("login")
