87c80389dfe99fb7997c6f1e377a09b50badcd02
iamEJ <jukneviciute185@gmail.com>
2020-07-21 14:48:44 +0300

    add basic logging for project

--- src/main/java/lt/management/oms/service/ProjectServiceImpl.java
+++ src/main/java/lt/management/oms/service/ProjectServiceImpl.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import lt.management.oms.exceptions.ProjectNotFoundException;
 import lt.management.oms.model.Project;
 import lt.management.oms.repository.ProjectRepository;
 
 @Service
+@Slf4j
 public class ProjectServiceImpl implements ProjectService {
 
 	@Autowired
@@ -17,21 +19,25 @@ public class ProjectServiceImpl implements ProjectService {
 
 	@Override
 	public List<Project> getAll() {
+		log.info("Returned all the projects");
 		return projectRepository.findAll();
 	}
 
 	@Override
 	public Project findById(Long id) {
+		log.info("The project found by id: {}", id);
 		return projectRepository.findById(id).orElseThrow(ProjectNotFoundException::new);
 	}
 
 	@Override
 	public Project createProject(Project project) {
+		log.info("The project successfully created");
 		return projectRepository.save(project);
 	}
 
 	@Override
 	public void deleteProjectById(Long id) {
+		log.info("The project deleted by id: {}", id);
 		projectRepository.deleteById(id);
 	}
 
@@ -43,6 +49,7 @@ public class ProjectServiceImpl implements ProjectService {
 		oldProject.setBudget(newProject.getBudget());
 		oldProject.setAddress(newProject.getAddress());
 		oldProject.setDeadline(newProject.getDeadline());
+		log.info("The project successfully updated");
 		return projectRepository.save(oldProject);
 	}
 }
