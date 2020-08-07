fe2d5154f97699f1922d9548329fa83071e415ec
Paulius <paulius.gaizauskas@gmail.com>
2020-08-07 15:21:52 +0300

    Remove unused imports

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,20 +1,10 @@
 import React, { useState, useCallback } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Copyright from "../components/Copyright";
 import Test from "../components/Test";
 import axios from "axios";
-import { Form, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Button, Form, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import Auth from "../components/auth/Authentication";
 
 const BASE_URL = "http://localhost:8080";
